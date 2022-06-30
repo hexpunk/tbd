76dc60a63e5f1cbb9a28b482f6535c6771432ac7
hexpunk <10983817+hexpunk@users.noreply.github.com>
2022-06-30 00:30:43 -0500

    Make a separate vendor chunk on build

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      manualChunks(id) {
+        if (id.includes("node_modules")) {
+          return "vendor";
+        }
+      },
+    },
+  },
   plugins: [preact()],
 });
