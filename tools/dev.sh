a622bd21084d2fdcef959d30649f927dacfba6f1
hexpunk <10983817+hexpunk@users.noreply.github.com>
2022-07-02 15:24:24 -0500

    Prepare dev environment for PHP

--- tools/dev.sh
+++ tools/dev.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
-APP_PATH="$(dirname "$0")/../"
-CONTAINER_NAME="tbd-dev"
-ENTRYPOINT="/bin/bash"
+MY_PATH=$(dirname "$0")
+APP_PATH="$MY_PATH/../"
+IMAGE_NAME="tbd-dev-env"
+CONTAINER_NAME=$IMAGE_NAME
+
+if ! podman images --format "{{.Repository}}" | grep -q $IMAGE_NAME; then
+  podman build --squash-all -t $IMAGE_NAME -f "$MY_PATH/Dockerfile"
+fi
 
 if podman ps --format "{{.Names}}" | grep -q $CONTAINER_NAME; then
-  podman exec -it $CONTAINER_NAME $ENTRYPOINT
+  podman exec -it $CONTAINER_NAME /bin/sh
 else
  podman run -it --rm \
-  --entrypoint $ENTRYPOINT \
   --name $CONTAINER_NAME \
   -p "127.0.0.1:3000:3000/tcp" \
   -v "$APP_PATH:/root/app/" \
-  -w "/root/app" \
-  node:18
+  $IMAGE_NAME
 fi
