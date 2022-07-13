9d419f80da51c424bb27723267ac4eafe9a1e1f7
hexpunk <10983817+hexpunk@users.noreply.github.com>
2022-07-13 17:29:15 -0500

    DRY up CSS config

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,5 @@
+const tailwindConfig = require("./tailwind.config");
+
 /** @type {import('postcss').Config} */
 module.exports = {
   plugins: {
@@ -6,7 +8,7 @@ module.exports = {
     ...(process.env.NODE_ENV === "production"
       ? {
           "@fullhuman/postcss-purgecss": {
-            content: ["./index.html", "./src/**/*.{ts,tsx}"],
+            content: tailwindConfig.content,
             defaultExtractor: (content) =>
               content.match(/[\w-/:.]+(?<!:)/g) || [],
           },
